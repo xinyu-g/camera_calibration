13192d55de7ec20d0acb20557615d17191e374f2
xinyu-g <xinyuccyy@gmail.com>
2021-12-27 17:22:35 -0800

    initial commit

---
+++ CHARUCO_calibration.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 20:58:09 2019
+
+@author: xinyu
+"""
+
+import numpy as np
+import cv2, PIL, os
+from cv2 import aruco
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+#%matplotlib nbagg
